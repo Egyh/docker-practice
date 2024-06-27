22d683a5d026c31cba739ac7b764073695d461c8
Egyh <yuhamaguchi18@gmail.com>
2024-06-28 01:14:42 +0900

    change: railsアプリケーションのみでdevcontainerを立ち上げるように変更

--- rails-container/.devcontainer/entrypoint.sh
+++ rails-container/.devcontainer/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# プロジェクトディレクトリに移動
+cd /workspace
+
+# 初回起動時に rails new を実行
+if [ ! -d /workspace/app ]; then
+  rails new . --force --skip-bundle
+  bundle install
+fi
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /rails-container/tmp/pids/server.pid
 
